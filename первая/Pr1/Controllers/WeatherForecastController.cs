b892743f6f23514b8ca965a7c6b35e88fc734d70
BolAnastasiya <125025551+BolAnastasiya@users.noreply.github.com>
2023-02-10 15:48:18 +0300

    Add files via upload

---
+++ первая/Pr1/Controllers/WeatherForecastController.cs
@@ -0,0 +1,92 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Pr1.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static List<string> Summaries = new()
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public List<string> Get()
+        {
+            return Summaries;
+        }
+        [HttpPost]
+        public IActionResult Add(string name)
+        {
+            Summaries.Add(name);
+            return Ok();
+        }
+        [HttpPut]
+        public IActionResult Update(int index, string name)
+        {
+            if(index<0 || index >=Summaries.Count)
+            {
+                return BadRequest("Неверный индекс!"); 
+            }
+            Summaries[index] = name;
+            return Ok();
+        }
+        [HttpDelete]
+        public IActionResult Delete(int index)
+        {
+            if (index < 0 || index >= Summaries.Count)
+            {
+                return BadRequest("Неверный индекс!");
+            }
+            Summaries.RemoveAt(index);
+            return Ok();
+        }
+        [HttpGet("{index}")]
+        public string GetIndex(int index)
+        {
+            if (index > 0 || index <= Summaries.Count)
+            {
+                return Summaries[index];
+            }
+            return "Неверный индекс!";
+        }
+        [HttpGet("find-by-name")]
+        public int GetString(string name)
+        {
+            return Summaries.Count(a => a == name);
+        }
+        [HttpGet("sort")]
+        public IActionResult GetAll(int? sortStrategy)
+        {
+            if (sortStrategy == null) return Ok(Summaries);
+            if (sortStrategy == 1)
+            {
+                Summaries.Sort();
+                return Ok(Summaries);
+            }    
+            if (sortStrategy == -1)
+            {
+                Summaries.Sort();
+                Summaries.Reverse();
+                return Ok(Summaries);
+            }
+            else
+            {
+                return BadRequest("Некорректное значение параметра sortStrategy");
+            }
+        }
+    }
+}
